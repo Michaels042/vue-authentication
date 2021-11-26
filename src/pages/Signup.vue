0369905d04fb289a7eea4b101c70dd8b382bb33e
michaels042 <mnwachukwu042@gmail.com>
2021-11-26 13:48:53 +0100

    first commit

---
+++ src/pages/Signup.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+        <div>
+        <div>
+            <h3>Signup</h3>
+            <hr/>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" class="form-control" />
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" />
+        </div>
+
+        <div class="my-3">
+            <button type="submit" class="btn btn-success">Signup</button>
+        </div>
+
+    </div>
+    </div>
+</div>
+    
+</template>
+<script>
+    export default{};
+</script>
